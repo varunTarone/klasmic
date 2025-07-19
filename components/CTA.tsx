152cb0ade75011cdc5679817312b97b113f46785
varunTarone <var17t@gmail.com>
2025-07-19 20:09:46 +0530

    feat: implement companion list and CTA

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section className='cta-section'>
+      <div className='cta-badge'>
+        Start learning your way!
+      </div>
+      <h2 className='text-3xl font-bold'>
+        Build and Customize AI Companion Today!
+      </h2>
+      <p>
+        Pick a name, subject, voice & personality for your AI companion - and start learning through voice conversations.
+      </p>
+      <Image src="/images/cta.svg" alt="AI Companion" width={362} height={232} />
+      <button className='btn-primary'>
+        <Image src="/icons/plus.svg" alt="Plus Icon" width={12} height={12} />
+        <Link href="/companions/new" className='ml-2'>Create Companion</Link>
+      </button>
+    </section>
   )
 }
 
--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -7,7 +7,10 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { cn } from "@/lib/utils";
+import { subjects } from "@/constants";
+import { cn, getSubjectColor } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
 
 interface CompanionsListProps{
   title: string;
@@ -28,11 +31,59 @@ const CompanionsList = ({ title, companions, classNames}: CompanionsListProps) =
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-          </TableRow>
+          {companions?.map(({id, subject, name, topic, duration}) => (
+            <TableRow key={id}>
+              <TableCell className="font-medium">
+                <Link href={`/companions/${id}`}>
+                  <div className="flex items-center gap-2">
+                    <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden" style={{backgroundColor: getSubjectColor(subject)}}>
+                      <Image
+                        src={`/icons/${subject}.svg`}
+                        alt={name}
+                        width={35}
+                        height={35}
+                        className="rounded-full"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <p className="font-bold text-2xl">
+                        {name}
+                      </p>
+                      <p className="text-lg">
+                        {topic}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              </TableCell>
+              <TableCell>
+                <div className="subject-badge w-fit max-md:hidden">
+                  {subject}
+                </div>
+                <div className="flex items-center justify-center rounded-lg w-fit p-2 md:hidden" style={{backgroundColor: getSubjectColor(subject)}}>
+                  <Image
+                    src={`/icons/${subject}.svg`}
+                    alt={subject}
+                    width={18}
+                    height={18}
+                  />
+                </div>
+              </TableCell>
+              <TableCell className="flex items-center gap-2 justify-end w-full">
+                <p className="text-2xl">
+                  {duration} 
+                  <span className="max-md:hidden">mins</span>  
+                </p>
+                  <Image
+                    src={`/icons/clock.svg`}
+                    alt={subject}
+                    width={14}
+                    height={14}
+                    className="md:hidden"
+                  />
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </article>
