71aa1c50502b84e984d72cac06cad8ce352f2878
varunTarone <var17t@gmail.com>
2025-07-23 23:42:03 +0530

    fix: samll change fix

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from '@clerk/nextjs'
-
+export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <main className='flex items-center justify-center'>
